7ec4492f8faac98e9d1928ed42e2b6d7237049ea
Andrew Kralovec <andrew.kralovec@yum.com>
2025-10-24 15:35:07 -0500

    0.1.0

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .version = .{
             .major = 0,
-            .minor = 0,
+            .minor = 1,
             .patch = 0,
         },
     });
