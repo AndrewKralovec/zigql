f7100ce7f98cf2629594c8d50f49a0a5fe06db54
Andrew Kralovec <andrew.kralovec@yum.com>
2026-02-26 21:19:44 -0600

    chore: rename

--- src/validator/validations/directives.zig
+++ src/validator/validations/directives.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 const ast = @import("../../grammar/ast.zig");
 const ValidationContext = @import("../validation_context.zig").ValidationContext;
 
-const checkUniqueArgs = @import("./argument.zig").checkUniqueArgs;
+const validateArguments = @import("./argument.zig").validateArguments;
 
 pub fn validateDirectives(ctx: *ValidationContext, directives: ?[]const ast.DirectiveNode) !void {
     const dirs = directives orelse return;
     for (dirs) |directive| {
-        try checkUniqueArgs(ctx, directive.arguments);
+        try validateArguments(ctx, directive.arguments);
     }
 }
--- src/validator/validations/selection.zig
+++ src/validator/validations/selection.zig
@@ -2,15 +2,14 @@ const std = @import("std");
 const ast = @import("../../grammar/ast.zig");
 const ValidationContext = @import("../validation_context.zig").ValidationContext;
 
-const checkUniqueArgs = @import("./argument.zig").checkUniqueArgs;
+const validateArguments = @import("./argument.zig").validateArguments;
 const validateDirectives = @import("./directives.zig").validateDirectives;
 
 pub fn validateSelectionSet(ctx: *ValidationContext, sel_set: ast.SelectionSetNode) !void {
     for (sel_set.selections) |sel| {
         switch (sel) {
             .Field => |field| {
-                // UniqueArgumentNamesRule
-                try checkUniqueArgs(ctx, field.arguments);
+                try validateArguments(ctx, field.arguments);
                 try validateDirectives(ctx, field.directives);
                 if (field.selection_set) |nested| {
                     try validateSelectionSet(ctx, nested);
