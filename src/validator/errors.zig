4dcf828f9c43a2ecdf17af1e0b5fca4e45b08854
Andrew Kralovec <andrew.kralovec@yum.com>
2026-02-26 21:42:28 -0600

    docs: name errors

--- src/validator/errors.zig
+++ src/validator/errors.zig
@@ -2,11 +2,44 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const ValidationErrorKind = enum {
+    /// Executable definitions
+    ///
+    /// A GraphQL document is only valid for execution if all definitions are either
+    /// operation or fragment definitions.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Executable-Definitions
     NonExecutableDefinition,
+    /// Lone anonymous operation
+    ///
+    /// A GraphQL document is only valid if when it contains an anonymous operation
+    /// (the query short-hand) that it contains only that one operation definition.
+    ///
+    /// See https:///spec.graphql.org/draft/#sec-Lone-Anonymous-Operation
     ManyAnonymousOperations,
+    /// Unique operation names
+    ///
+    /// A GraphQL document is only valid if all defined operations have unique names.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Operation-Name-Uniqueness
     DuplicateOperationName,
+    /// Unique fragment names
+    ///
+    /// A GraphQL document is only valid if all defined fragments have unique names.
+    //
+    /// See https://spec.graphql.org/draft/#sec-Fragment-Name-Uniqueness
     DuplicateFragmentName,
+    /// Unique variable names
+    ///
+    /// A GraphQL operation is only valid if all its variables are uniquely named.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Variable-Uniqueness
     DuplicateVariableName,
+    /// Unique argument names
+    ///
+    /// A GraphQL field or directive is only valid if all supplied arguments are
+    /// uniquely named.
+    ///
+    /// See https://spec.graphql.org/draft/#sec-Argument-Names
     DuplicateArgumentName,
 };
 
