1359d1a2eee964dc5595d7b45f886e4339c6e726
Andrew Kralovec <andrew.kralovec@yum.com>
2026-03-14 21:28:15 -0500

    chore: stub out enum

---
+++ src/validator/validations/enum.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const ast = @import("../../grammar/ast.zig");
+const ValidationContext = @import("../validation_context.zig").ValidationContext;
+
+pub fn validateEnumDefinition(ctx: *ValidationContext, union_def: ast.UnionTypeDefinitionNode) std.mem.Allocator.Error!void {
+    _ = ctx;
+    _ = union_def;
+    // TODO: add validation logic
+}
+
+pub fn validateEnumValue(ctx: *ValidationContext, union_def: ast.UnionTypeDefinitionNode) std.mem.Allocator.Error!void {
+    _ = ctx;
+    _ = union_def;
+    // TODO: add validation logic
+}
